786885a0fae75d59e468310bf395b1cc34849d39
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-04-30 16:47:32 +0000

    Added action enums

--- src/vaultData/log/Log.ts
+++ src/vaultData/log/Log.ts
@@ -1,11 +1,40 @@
 import { EncData } from "../encrypted/EncData";
 import { DataID, DataType, LogID, UserID } from "../ID";
 
-export type Log<T extends DataType> = {
+export enum FieldAction{
+	Created,
+	Updated,
+	Deleted
+}
+
+export enum ItemAction{
+	Created,
+	Updated,
+	Deleted,
+	Moved,
+	Copied
+}
+
+export enum GroupAction{
+	Created,
+	Updated,
+	Deleted,
+	UserAdded,
+	UserRemoved,
+}
+
+export type Action = {
+	[DataType.Field]: FieldAction;
+	[DataType.Group]: GroupAction;
+	[DataType.Item]: ItemAction;
+}
+
+export type Log<T extends keyof Action> = {
 	id: LogID;
 	timestamp: Date;
 	dataType: T;
 	objid: DataID<T>;
 	data: T extends keyof EncData ? EncData[T] : null;
 	user: UserID | null;
+	action: Action[T];
 }
