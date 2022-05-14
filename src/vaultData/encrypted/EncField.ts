65ada5cac05c869903a16c5c87b306f417e81db4
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-05-14 15:34:40 +0000

    NewItem and NewField type

--- src/vaultData/encrypted/EncField.ts
+++ src/vaultData/encrypted/EncField.ts
@@ -1,7 +1,9 @@
 import { SymEnc, SymEncType } from "../../encryption/SymEnc";
-import { FieldID } from "../ID";
+import { FieldID, TempFieldID } from "../ID";
 
-export type EncField = {
-    id: FieldID;
+export type EncFieldData = {
     data: SymEnc<SymEncType.FieldData>;
 }
+
+export type EncField = EncFieldData & {id: FieldID};
+export type NewEncField = EncFieldData & {id: TempFieldID};
--- src/vaultData/encrypted/EncItem.ts
+++ src/vaultData/encrypted/EncItem.ts
@@ -1,12 +1,14 @@
 import { PartialPartial } from "../../base/PartialPartial";
 import { SymEnc, SymEncType } from "../../encryption/SymEnc";
-import { ContID, ItemID } from "../ID";
+import { ContID, ItemID, TempItemID } from "../ID";
 
 export type EncItemDelta = PartialPartial<EncItem, 'id', never>;
 
-export type EncItem = {
-	id: ItemID;
+export type EncItemData = {
 	data: SymEnc<SymEncType.ItemData>;
 	encKey: SymEnc<SymEncType.ItemKey>;
 	container: ContID;
 }
+
+export type EncItem = EncItemData & {id: ItemID};
+export type NewEncItem = EncItemData & {id: TempItemID};
